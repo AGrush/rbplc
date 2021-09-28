206ad0121e4104754833c6a35a383be319d2e3e4
AGrush <grushevskiy@gmail.com>
2021-09-28 12:38:42 +0100

    .

--- functions.php
+++ functions.php
@@ -17,13 +17,13 @@ require_once('lib/theme-support.php');
 require_once('lib/navigation.php');
 
 //delete-posts
-require_once('lib/delete-post.php');
+// require_once('lib/delete-post.php');
 
 //metaboxes - coming from a plugin now
 //require_once('lib/metaboxes.php');
 
 //ask to activate necessary plugins we've built using tgm plugin activator here, add new plugin paths in here
-require_once('lib/include-plugins.php');
+// require_once('lib/include-plugins.php');
 
 //change comments markup
 // require_once('lib/comment-callback.php');
