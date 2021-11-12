df004b70c94455512efd3d020305ec4e447d8c0e
AGrush <grushevskiy@gmail.com>
2021-11-12 14:57:11 +0000

    finished final4

--- template-parts/page/share-price-center.php
+++ template-parts/page/share-price-center.php
@@ -5,7 +5,7 @@
                 <iframe class="c-simple-page__brighterir-spwidget"
                 src="https://polaris.brighterir.com/public/revolution_beauty_group/data_table/share_price_widget"></iframe>
             </div>
-            <h1>share price center</h1>
+            <h1>share price centre</h1>
         </div>
         <iframe class="c-simple-page__brighterir-spinfo"
         src="https://polaris.brighterir.com/public/revolution_beauty_group/tool_group/share_price_information"></iframe>
